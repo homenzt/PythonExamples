937f1c5eb734914464f03d4c35f41d06a1a631db
Zachary Homen <z.homen.95@gmail.com>
2020-09-08 18:05:07 -0600

    Add files via upload

---
+++ Q018/main.py
@@ -0,0 +1,43 @@
+# ================================================================================== 
+#
+# Author: 	Zachary Homen
+# Date:		09/08/2020
+#
+# ==================================================================================
+#
+# A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
+#
+# Following are the criteria for checking the password:
+#
+# At least 1 letter between [a-z]
+# At least 1 number between [0-9]
+# At least 1 letter between [A-Z]
+# At least 1 character from [$#@]
+# Minimum length of transaction password: 6
+# Maximum length of transaction password: 12
+# Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+#
+# Example
+#
+# If the following passwords are given as input to the program:
+#
+# ABd1234@1,a F1#,2w3E*,2We3345
+#
+# Then, the output of the program should be:
+#
+# ABd1234@1
+#
+# ==================================================================================
+
+import re
+
+def main():
+	password = input().split(',')
+
+	for p in password:
+		match = re.search(r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[$#@]).{6,12}$",p)
+		if match:
+			print(match.group())
+
+if __name__=="__main__":
+	main()
